de105216ffba90b7d64d5561d8e9782a9c381963
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-08-20 15:10:18 -0300

    Login page without layout

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,10 +1,12 @@
+import AppRouter from "./router/AppRouter"
+import AppTheme from "./theme/AppTheme"
 
 
 const JournalApp = () => {
   return (
-    <>
-        <h1>JournalApp</h1>
-    </>
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
   )
 }
 
---
+++ src/journal/pages/JorunalPage.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+
+const JorunalPage = () => {
+  return (
+    <>
+      <Typography variant="h1">JournalPage</Typography>
+    </>
+  )
+}
+
+export default JorunalPage
