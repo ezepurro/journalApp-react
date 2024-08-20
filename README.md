74835ae1c89b52d7153a5775f67937695bdf31b5
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-08-19 21:13:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JournalApp 
+
+App hecha con React para practicar uso de
+
+
+- Material UI
+- Redux
+- Autenticación en Firebase
---
+++ src/JournalApp.jsx
@@ -0,0 +1,11 @@
+
+
+const JournalApp = () => {
+  return (
+    <>
+        <h1>Hola mundo!</h1>
+    </>
+  )
+}
+
+export default JournalApp
